25be4159479e711d21662594ab64f2e67a250679
Quipex <rudas72@gmail.com>
2021-05-23 00:13:19 +0300

    initial commit

---
+++ src/test/java/com/binarystudio/academy/springsecurity/SpringSecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarystudio.academy.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
