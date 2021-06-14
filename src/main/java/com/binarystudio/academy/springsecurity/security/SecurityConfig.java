47b83937a5610db9adce2596174e32020904a75e
Quipex <rudas72@gmail.com>
2021-06-15 01:30:19 +0300

    OAuth2

--- src/main/java/com/binarystudio/academy/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/binarystudio/academy/springsecurity/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.binarystudio.academy.springsecurity.security;
 
 import com.binarystudio.academy.springsecurity.domain.user.model.UserRole;
 import com.binarystudio.academy.springsecurity.security.jwt.JwtFilter;
+import com.binarystudio.academy.springsecurity.security.oauth2.RedirectUriToCookiePersister;
+import com.binarystudio.academy.springsecurity.security.oauth2.SimpleOAuth2SuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,6 +13,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -47,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		applyRouteRestrictions(http);
+		applyOAuth2Config(http);
 
 		http.addFilterBefore(filterChainExceptionHandler(), UsernamePasswordAuthenticationFilter.class);
 		http.addFilterBefore(tokenFilter(), UsernamePasswordAuthenticationFilter.class);
@@ -56,13 +61,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return exceptionHandling -> exceptionHandling.authenticationEntryPoint((request, response, authException) -> response.sendError(403));
 	}
 
+	private void applyOAuth2Config(HttpSecurity http) throws Exception {
+		http
+				.oauth2Login(oauth2Config -> oauth2Config
+						.authorizationEndpoint(auth -> {
+							auth.baseUri("/auth/oauth2/authorize");
+							auth.authorizationRequestRepository(authorizationRequestRepository());
+						})
+						// You also need to ensure the client.registration.redirectUri (at application.yml) matches the custom
+						// Authorization Response baseUri.
+						.redirectionEndpoint(redir -> redir.baseUri("/auth/oauth2/code/*"))
+						.successHandler(oAuth2SuccessHandler()));
+	}
+
 	@Bean
 	public JwtFilter tokenFilter() {
 		return new JwtFilter();
 	}
 
 	@Bean
+	public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
+		return new RedirectUriToCookiePersister();
+	}
+
+	@Bean
 	public FilterChainExceptionHandler filterChainExceptionHandler() {
 		return new FilterChainExceptionHandler();
+	}
+
+	@Bean
+	public SimpleOAuth2SuccessHandler oAuth2SuccessHandler() {
+		return new SimpleOAuth2SuccessHandler();
 	}
 }
---
+++ src/main/java/com/binarystudio/academy/springsecurity/security/oauth2/SimpleOAuth2UserService.java
@@ -0,0 +1,31 @@
+package com.binarystudio.academy.springsecurity.security.oauth2;
+
+import com.binarystudio.academy.springsecurity.domain.user.UserRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleOAuth2UserService extends DefaultOAuth2UserService {
+	private final UserRepository userRepository;
+
+	public SimpleOAuth2UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		var oAuth2User = super.loadUser(userRequest);
+		String email = oAuth2User.getAttribute("email");
+		var foundUser = userRepository.findByEmail(email);
+		if (foundUser.isPresent()) {
+			// update user, verify his email, etc since the email is returned from trusted source
+		} else {
+			// here you can properly register the user
+			userRepository.createUserByEmail(email);
+		}
+		return oAuth2User;
+	}
+}
